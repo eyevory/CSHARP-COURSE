64e1bd6bb080283c171ef1c8732e801d3c718fd0
Linus Nordqvist <71812586+eyevory@users.noreply.github.com>
2024-11-19 10:59:32 +0200

    Update Program.cs

--- Assignments/A.2 Conversion in C#/Program.cs
+++ Assignments/A.2 Conversion in C#/Program.cs
@@ -6,10 +6,11 @@ namespace ConversionTaskInCSHARP
     {
         static void Main(string[] args)
         {
-
+            // Declare string variable with numerical value
             string numString = "41";
+            // Convert string value to new int variable
             int num = int.Parse(numString);
-
+            // Write out converted int variable
             Console.WriteLine(num);
 
             Console.ReadKey();
