60c755385b9dd665c3413af2267d10ea006a25c8
Linus Nordqvist <71812586+eyevory@users.noreply.github.com>
2024-11-19 10:57:13 +0200

    Update Program.cs

--- Assignments/A.1 Variables in C#/Program.cs
+++ Assignments/A.1 Variables in C#/Program.cs
@@ -6,11 +6,12 @@ namespace VariablesInCSHARP
     {
         static void Main(string[] args)
         {
-
+            // Variables
             var name = "Linus";
             var num = 7;
             var num2 = 3;
 
+            // Writes out each variable in their own line
             Console.WriteLine(name + "\n" + num + "\n" + num2);
 
             Console.ReadKey();
