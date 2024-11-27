85d17310e346a47bf4af341b469826743a5adc55
Linus Nordqvist <71812586+eyevory@users.noreply.github.com>
2024-11-27 13:19:03 +0200

    extra assignments

---
+++ Assignments/A.CM Days until Christmas day/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.CM_Days_until_Christmas_day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables for current day and this years christmas day
+            DateTime today = DateTime.Today;
+            DateTime christmasDay = new DateTime(2024, 12, 25);
+
+            // Loop that calculates days left to christmas and prints it
+            while (true)
+            {
+                if (today < christmasDay)
+                {
+                    TimeSpan untilChristmasDay = christmasDay - today;
+                    Console.Write("Days until christmas day: ");
+                    string convertChristmasDay = untilChristmasDay.ToString();
+                    string trimmedChristmasDay = convertChristmasDay.Remove(convertChristmasDay.Length - 9); // Remove last 9 characters to only show days in clean format
+                    Console.WriteLine(trimmedChristmasDay);
+                    break;
+                }
+                else // Add one year to Christmas day in case the date has been passed
+                {
+                    christmasDay = christmasDay.Date.AddYears(1);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignments/A.EX Jens Food List/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.EX_C__Food_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create list
+            List<string> shoppingList = new List<string>();
+            
+            // bool to use in while loop
+            bool stop = true;
+            
+            // while loop to add input to list
+            while (stop)
+            {
+                Console.Write("Enter item to add to shopping list (input \"stop\" when done): ");
+                shoppingList.Add(Console.ReadLine());
+                if (shoppingList.Contains("stop")) // When user inputs stop to end the loop
+                    {
+                    shoppingList.Remove("stop");
+                    stop = false;
+                    }
+                    
+            }
+
+            Console.WriteLine("\nYour shopping list: ");
+
+            // for loop to print out list after use input
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
